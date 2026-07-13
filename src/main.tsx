9ab798f6c83ed0dcf6c08ba3e6898d93dd2e8f81
JHN-MAD <hnjung@madup.com>
2026-07-13 19:42:22 +0900

    chore: 프런트엔드 개발 진단 도구 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+if (import.meta.env.DEV) {
+  void import("react-grab");
+  void import("react-scan");
+}
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(globalThis.document.getElementById("root") as globalThis.HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
